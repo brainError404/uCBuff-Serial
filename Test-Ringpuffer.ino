749b890c6fda8d6350ccf7b986cc8a617923b206
brainError404 <45049288+brainError404@users.noreply.github.com>
2018-11-21 20:44:04 +0100

    Add files via upload
    Data is saved in buffer
    No closed loop control for ring buffer storage

---
+++ Test-Ringpuffer.ino
@@ -0,0 +1,290 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+const int dataLength = 7;
+
+volatile byte cSIndex = 99;
+
+int dgToRead = 1;                           // determines how many datagrams per intervall should be read to regulate the buffers capacity
+int bytesAvailable = 0;                     // how many datagrams are ready to read from serial
+int dgToReadNow = 1;                        // how many datagrams will be read this cycle from serial
+
+byte telBuffFull[] = {'b', 77, 77, 77, 77, 77, 77};
+byte telBuffEmpty[] = {'b', 1, 1, 1, 1, 1, 1};
+byte telTest[] = {'t', 88, 88, 88, 88, 88, 88};
+
+enum states {
+  PREPARE_BUF,
+  MOVING,
+  PAUSED
+};
+
+int state = PREPARE_BUF;                    // fill the buffer before starting
+
+// ringbuffer
+typedef struct
+{
+  int * const buff;
+  int head;
+  int tail;
+  const int maxLen;
+} circBuf_t;
+
+#define CIRCBUF_DEF(x,y)        \
+  int x##_space[y];             \
+  circBuf_t x = {               \
+                                .buff = x##_space,        \
+                                .head = 0,                \
+                                .tail = 0,                \
+                                .maxLen = y               \
+                }
+
+
+
+CIRCBUF_DEF(buf, 64 );      // Rinbuffer mit 128 Elementen anlegen
+const int buffThresLow = (int) (buf.maxLen * 0.2);
+const int buffThresHalf = (int) (buf.maxLen * 0.5);
+const int buffThresHigh = (int) (buf.maxLen * 0.8);
+
+int push(circBuf_t *c, byte data)
+{
+  // next is where head will point to after this write.
+  byte next = c->head + 1;
+  if (next >= c->maxLen) {
+    next = 0;
+  }
+
+
+  if (next == c->tail) {    // check if circular buffer is full
+    return -1;              // and return with an error.
+  }
+
+  c->buff[c->head] = data;  // Load data and then move
+  c->head = next;           // head to next data offset.
+  return 0;                 // return success to indicate successful push.
+}
+
+int pop(circBuf_t *c, byte *data)
+{
+  // if the head isn't ahead of the tail, we don't have any characters
+  if (c->head == c->tail - 1)   // check if circular buffer is empty
+    return -1;              // and return with an error
+
+
+
+  // next is where tail will point to after this read.
+  int next = c->tail + 1;
+  if (next >= c->maxLen)
+    next = 0;
+
+  *data = c->buff[c->tail]; // Read data and then move
+  c->tail = next;           // tail to next data offset.
+  return 0;                 // return success to indicate successful push.
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  digitalWrite(LED_BUILTIN, LOW);
+
+}
+
+int cnt = 0;
+
+byte data = 0;
+byte val = 0;
+
+void loop() {
+
+  switch (state) {
+    case PREPARE_BUF:
+      PrepareBuffer();
+      state = MOVING;
+      break;
+
+    case MOVING:
+
+      // only send data when we received them and scara should move
+      while (state == MOVING) {
+
+        // check capacity of buffer
+        if (GetBytesInBuff(buf) < buffThresLow) {
+          // notify pc that ringbuffer is going to become empty
+          WriteDatagram(telBuffEmpty);
+          //dgToRead = 2;
+        }
+        if (GetBytesInBuff(buf) > buffThresHigh) {
+          // notify that buffer is close to full
+          WriteDatagram(telBuffFull);
+          //dgToRead = 0;
+        }
+
+        // pop data from buffer to serial
+        for (int i = 0; i < dataLength; i++) {
+          pop(&buf, &data);
+          // cancel transaction, cause we received the last datagram
+          if ((i == 0) &&  (data == 'e')) {
+
+            state = PREPARE_BUF;
+
+            // reset the ring buffer
+            buf.head = 0;
+            buf.tail = 0;
+
+            break;
+          }
+
+          Serial.write(data);
+
+        }
+
+        //GetBuffInfos(telTest, &buf);
+        //WriteDatagram(telTest);
+
+        // check if theres enough data in serial rx buffer
+        /*bytesAvailable = Serial.available();
+          if (bytesAvailable >= dgToRead * 7) {
+          // read all the required datagrams this cycle
+          dgToReadNow = dgToRead;
+          // reset the regulator value
+          dgToRead = 1;
+          }
+          // if theres not enough data available read as much as possible
+          // and read the rest next cycle
+          else {
+          dgToReadNow = bytesAvailable;
+          // read next cycle the difference plus the new incoming datagram
+          dgToRead = dgToRead - dgToReadNow + 1;
+
+          /*telTest[1] = (byte)bytesAvailable;
+          telTest[2] = (byte)dgToRead * 7;
+          telTest[3] = (byte)buf.head;
+          telTest[4] = (byte)buf.tail;
+          telTest[5] = (byte)abs(buf.head - buf.tail);
+          telTest[6] = (byte)GetBytesInBuff(buf);
+          WriteDatagram(telTest);
+          }*/
+        dgToReadNow = 1;
+
+        // read another datagramm from serial port
+        if (state == MOVING && (dgToRead > 0)) {
+          // read more ore less than one datagram if the buffers storage (-> capacity) is to much or less
+          for (int i = 0; i < dgToReadNow; i++) {
+
+            if (Serial.available() >= dataLength) {
+              for (int j = 0; j < dataLength; j++) {
+
+                // check for pause call sign
+                if (j == 0) {
+                  val = Serial.read();
+                  // pause all action if requested
+                  if (val == 'p') {
+                    state = PAUSED;
+                    break;
+                  }
+                  else {
+                    push(&buf, val);
+                  }
+                }
+                else {
+                  push(&buf, Serial.read());
+                }
+              }
+            }
+            else {
+              // send infos about ring and rx buffer
+              telTest[6] = (byte) Serial.available();
+              GetBuffInfos(telTest, &buf);
+              WriteDatagram(telTest);
+            }
+
+          }
+        }
+
+        delay(50);
+      }
+      break;
+
+    case PAUSED:
+      WaitForResume();
+      state = MOVING;
+      break;
+  }
+}
+
+
+
+
+
+void WriteDatagram(byte *datagram) {
+  for (int i = 0; i < dataLength; i++) {
+    Serial.write(*(datagram + i));
+  }
+}
+
+int GetBytesInBuff(circBuf_t buff) {
+  int diff = buff.head - buff.tail;
+  diff = (diff < 0) ? buff.maxLen - buff.tail + buff.head : diff;
+  return diff;
+}
+
+void GetBuffInfos(byte *telBuffEmpty, circBuf_t *buff) {
+  static int index = 0;
+  *(telBuffEmpty + 1) = (byte)index++;
+  *(telBuffEmpty + 2) = (byte)GetBytesInBuff(*buff);
+  *(telBuffEmpty + 3) = (byte)buffThresLow;
+  *(telBuffEmpty + 4) = (byte)buff->head;
+  *(telBuffEmpty + 5) = (byte)buff->tail;
+}
+
+// checks if buffer is half full before starting
+void PrepareBuffer() {
+
+  int cnt = 0;                                    // byte index of datagram
+  bool firstByteValid = false;                    // true if recceived call sign for starting
+
+  // check capacity of buffer after receiving a whole datagram
+  while (GetBytesInBuff(buf) < buffThresHalf) {
+    // ensure that only whole datagráms are filled into the buffer
+    while (cnt < dataLength) {
+      // only read data if available
+      if (Serial.available()) {
+        // only fill buffer if first call sign was valid
+        if (firstByteValid) {
+          // fill the buffer
+          push(&buf, Serial.read());
+          cnt++;
+        }
+        // first call sign must be an 's'
+        else if (Serial.read() == 's') {
+          firstByteValid = true;
+          push(&buf, 's');
+          cnt++;
+        }
+      }
+    }
+    // reset the datagram index
+    cnt = 0;
+  }
+}
+
+void WaitForResume() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  while (state == PAUSED) {
+    if (Serial.available()) {
+      if (Serial.read() == 'p') {
+        // read the whole endPause datagram but dont save it
+        for (int i = 0; i < (dataLength - 1); i++) {
+          while (!Serial.available());
+          Serial.read();
+        }
+        break;
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+    }
+  }
+}
+
+
+
---
+++ uC_SerialBuff.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
+
+// Wenn übertragung unterbrochen wurde, muss wieder puffer arduino seitig auf 50% gefüllt werden
+// -> telegram während pausen senden
+// Kapazitätsregelung implementieren
+
+
+namespace SCARA_uC_Serial
+{
+    class Program
+    {
+        public static SerialPort _serialPort = new SerialPort();
+        //public static MySerialReader serialReader = new MySerialReader();
+        public const int dataLength = 7;
+        private static bool running = false;
+        private static bool bufferLow = false;
+        private static bool bufferHigh = false;
+        private static double telegramIntervall = 0.05;
+        private static List<byte[]> telegramList = new List<byte[]>();
+        private static List<byte[]> receivedDataList = new List<byte[]>();
+        private static Thread transmitThread = new Thread(TransmitData);
+        private static bool lastDataSent = false;
+        private static Queue<byte> receivedData = new Queue<byte>();
+        private static byte[] validCallSign = new byte[4] { Convert.ToByte('b'), Convert.ToByte('l'), Convert.ToByte('e'), Convert.ToByte('t') };
+        private static byte[] endTransaction = new byte[dataLength] { Convert.ToByte('e'), 0, 0, 0, 0, 0, 0 };
+        private static byte[] startTransaction = new byte[dataLength] { Convert.ToByte('s'), 0, 0, 0, 0, 0, 0 };
+        private static byte[] pauseTransaction = new byte[dataLength] { Convert.ToByte('p'), 0, 0, 0, 0, 0, 0 };
+        private static int buffThresLowDat = 3;             // indicates how many datagrams from the telegramList can be unsent
+        // in the end of a transaction before the uC notifies "bufferLow"                                    
+
+        static void Main(string[] args)
+        {
+            _serialPort.BaudRate = 115200;
+            _serialPort.PortName = "COM3";
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            Console.WriteLine("Anzahl der zu übertragenden Punkte eingeben:\t");
+            telegramList = CreateMockData(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("\nErzeugte Datagramme");
+            foreach (byte[] telegram in telegramList)
+            {
+                string telegramStr = "";
+                for (int i = 0; i < dataLength; i++)
+                {
+                    telegramStr += "\t" + telegram[i];
+                }
+                Console.WriteLine(telegramStr);
+            }
+
+            Console.WriteLine("\nUebertragung\n\ts zum Starten\n\te zum Beenden\n");
+            while (!lastDataSent)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "s":
+
+                        if (transmitThread.ThreadState == System.Threading.ThreadState.Suspended)
+                        {
+                            _serialPort.Write(pauseTransaction, 0, dataLength);
+                            transmitThread.Resume();
+                        }
+                        else
+                        {
+                            try
+                            {
+                                _serialPort.Open();
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message.ToString());
+                            }
+                            transmitThread.Start();
+
+                            running = true;
+                        }
+
+                        break;
+                    case "e":
+                        _serialPort.Write(pauseTransaction, 0, dataLength);
+                        transmitThread.Suspend();
+                        Console.WriteLine("Thread unterbrochen");
+                        break;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort serialPort = (SerialPort)sender;
+            // provisorisch.... muss grund herausfinden, warum 240 anfangs PCseitig gesendet wird
+            byte receivedCallSign = Convert.ToByte(serialPort.ReadByte());
+            // check for valid call sign
+            /*if (receivedCallSign == Convert.ToByte('t'))
+            {
+                ShowInvalidDatagram(serialPort,receivedCallSign,0);
+            }
+            */
+            switch (Convert.ToChar(receivedCallSign))
+            {
+                case 'b':
+                    byte secondByte = Convert.ToByte(serialPort.ReadByte());
+                    switch (secondByte)
+                    {
+                        case 0x01:
+                            bufferLow = true;
+                            //ReadRestOfDatagram(serialPort, 2);
+                            ShowInvalidDatagram(serialPort, Convert.ToByte(receivedCallSign), secondByte);
+                            break;
+                        case 0xFF:
+                            ShowInvalidDatagram(serialPort, Convert.ToByte(receivedCallSign),secondByte);
+                            break;
+                        default: ShowInvalidDatagram(serialPort, Convert.ToByte(receivedCallSign),secondByte);
+                            break;
+
+                    }
+                    break;
+                default:
+                    if (validCallSign.Contains(receivedCallSign))
+                    {
+                        receivedData.Enqueue(receivedCallSign);
+                        for (int i = 1; i < dataLength; i++)
+                        {
+                            receivedData.Enqueue(Convert.ToByte(serialPort.ReadByte()));
+                            ProcessReceivedData();
+                        }
+                    }
+                    else
+                    {
+                        ShowInvalidDatagram(serialPort, receivedCallSign, 0);
+                    }
+                    break;
+            }
+
+        }
+
+        private static void ShowInvalidDatagram(SerialPort serialPort, byte callSign, byte secondByte)
+        {
+            //Console.WriteLine("Ungültiges Datagramm");
+            string invalidDatagramStr = "\t" + callSign.ToString();
+            if (secondByte == 0)
+            {
+                for (int i = 1; i < dataLength; i++)
+                {
+                    invalidDatagramStr += "\t" + Convert.ToByte(serialPort.ReadByte());
+                }
+            }
+            else
+            {
+                invalidDatagramStr += "\t" + secondByte;
+                for (int i = 2; i < dataLength; i++)
+                {
+                    invalidDatagramStr += "\t" + Convert.ToByte(serialPort.ReadByte());
+                }
+            }
+
+            Console.WriteLine(invalidDatagramStr);
+        }
+
+        private static void ReadRestOfDatagram(SerialPort serialPort, int startIndex)
+        {
+            for (int i = startIndex; i < dataLength; i++)
+            {
+                serialPort.ReadByte();
+            }
+        }
+
+        private static void ProcessReceivedData()
+        {
+            if (receivedData.Count >= dataLength)
+            {
+                string receivedDatagramStr = "";
+                receivedDataList.Add(receivedData.ToArray());
+                foreach (byte b in receivedData)
+                {
+                    receivedDatagramStr += "\t" + b.ToString();
+                }
+                Console.WriteLine(receivedDatagramStr + "\t" + DateTime.UtcNow.AddHours(1).ToString("HH:mm:ss.fff"));
+                receivedData.Clear();
+            }
+        }
+
+        private static List<byte[]> CreateMockData(int amount)
+        {
+            Random rand = new Random();
+            int cnt = 0;
+            List<byte[]> telegramStorage = new List<byte[]>();
+            telegramStorage.Add(startTransaction);
+            for (int i = 0; i < amount; i++)
+            {
+                byte[] newPoint = new byte[dataLength];
+                newPoint[0] = Convert.ToByte('l');
+                newPoint[1] = Convert.ToByte(cnt++);
+                for (int j = 2; j < dataLength; j++)
+                {
+                    newPoint[j] = Convert.ToByte(rand.Next(10, 100));
+                }
+                telegramStorage.Add(newPoint);
+            }
+            telegramStorage.Add(endTransaction);
+            return telegramStorage;
+        }
+
+        private static void TransmitData()
+        {
+            //DateTime dateTime = new DateTime();
+            Stopwatch stopWatch = new Stopwatch();
+            //long lastTicks = 0;
+            int index = 0;
+            int datagramesToSend = 1;                               // send usually one datagram per intervall
+            bool firstLoop = true;
+
+            while (running)
+            {
+                if (firstLoop)
+                {
+                    Console.WriteLine("Starte Übertragung");
+                    stopWatch.Start();
+                    firstLoop = false;
+                }
+                // check if capacity of ringbuffer must be adjusted
+                // buffer naturally runs low when the last few datagrams are sent to uC -> controll if this is the case
+                if (bufferLow && ((index++) < (telegramList.Count - buffThresLowDat)))
+                {
+                    datagramesToSend = 2;
+                    Console.WriteLine("Reguliere Ringbuffer Kapazität +");
+                    bufferLow = false;
+                }
+                else if (bufferHigh)
+                {
+                    datagramesToSend = 0;
+                    bufferHigh = false;
+                }
+
+                if (stopWatch.ElapsedMilliseconds >= telegramIntervall * 1000)
+                {
+                    stopWatch.Restart();
+
+                    if (index < telegramList.Count)
+                    {
+                        for (int i = 0; i < datagramesToSend; i++)
+                        {
+                            _serialPort.Write(telegramList[index++], 0, dataLength);
+                        }
+                        datagramesToSend = 1;                               // reset to default;   
+                    }
+                    else
+                    {
+                        Console.WriteLine("Alle Datagramme verschickt!");
+
+                        // Test ob Methode CheckreceivedTelegrams funktioniert
+                        /*byte[] testTelegram = { 108, 34, 56, 1, 2, 3, 4 };
+                        receivedDataList.RemoveAt(receivedDataList.Count - 1;
+                        receivedDataList.Add(testTelegram);*/
+                        Thread.Sleep(1000);                 // wait for ringbuffer to send data
+                        CheckReceivedTelegrams();
+                        running = false;
+                        lastDataSent = true;
+                        _serialPort.Close();
+                    }
+                }
+            }
+        }
+
+        private static void CheckReceivedTelegrams()
+        {
+            if (receivedDataList.Count != telegramList.Count)
+            {
+                Console.WriteLine("\nMehr/weniger Datagramme empfangen als verschickt");
+                Console.WriteLine("{0} empfangen zu {1} gesendet", receivedDataList.Count, telegramList.Count);
+                int cnt = 0;
+                foreach (byte[] bArr in receivedDataList)
+                {
+                    if (bArr[1] != cnt++)
+                    {
+                        Console.WriteLine("Telegram mit Index {0} fehlt", --cnt);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < receivedDataList.Count; i++)
+                {
+                    for (int j = 0; j < dataLength; j++)
+                    {
+                        if ((receivedDataList[i][j] - telegramList[i][j]) != 0)
+                        {
+                            Console.WriteLine("Wert bei Telegramm: {0} Byte: {1} weicht ab", i, j);
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+    }
+}
